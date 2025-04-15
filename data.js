a21a718cb877982f693e8e413c49592e1e70448e
DevManTowhid <mdtowhidulalam364@gmail.com>
2025-04-15 10:36:26 +0000

    Add initial HTML structure and form elements for Cover Page Generator

--- data.js
+++ data.js
@@ -2,3 +2,8 @@ const headings = {
     top: "Heavens Light is Our Guide",
     next: "Rajshahi University of Engineering & Technology"
 }
+
+const reportTypes = [
+    "Lab Report", "Assignment", "Project", "Thesis", "Research Paper"
+
+]
